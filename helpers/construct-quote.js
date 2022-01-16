05476d21f5e453631cd7a953df3c4fb689a2e370
William <webuntermench@protonmail.com>
2022-01-17 01:29:07 +0300

    word wrap quote to string list

--- helpers/construct-quote.js
+++ helpers/construct-quote.js
@@ -1,5 +1,5 @@
 import { image } from "./ascii-image.js";
-const tab = `\t`;
+export const tab = ` `;
 
 function calculateTabulation(asciiImage, averageMain) {
   let longest = asciiImage.reduce((a, b) => {
@@ -7,32 +7,47 @@ function calculateTabulation(asciiImage, averageMain) {
   });
   const tabulation = {};
   if (asciiImage[averageMain].length >= longest.length) {
-    tabulation.mainString = 1;
-    tabulation.secondString = 3;
+    tabulation.mainString = 4;
+    tabulation.secondString = 16;
   } else {
-    tabulation.mainString = 2;
-    tabulation.secondString = 3;
+    tabulation.mainString = 8;
+    tabulation.secondString = 25;
   }
   return tabulation;
 }
 
-function prepareOutput() {
+export function prepareOutput() {
   const asciImage = image.split("\n");
   const average = Math.round(asciImage.length / 2);
   const { mainString, secondString } = calculateTabulation(asciImage, average);
   return { asciImage, average, mainString, secondString };
 }
 
-export function consoleImage(response) {
-  const { asciImage, average, mainString, secondString } = prepareOutput();
-  asciImage.forEach((el, index) => {
-    if (index === average) {
-      el += tab.repeat(mainString);
-      el += response.quote;
-    } else if (index === average + 2) {
-      el += tab.repeat(secondString);
-      el += response.character;
+export function splitQuote(data) {
+  const quoteLength = data.quote;
+  const average = Math.round(image.split("\n").length / 2);
+  const mocha = calculateTabulation(image.split("\n"), average);
+  const rowLength = image.split("\n")[average] + tab.repeat(mocha.mainString);
+  const remain = process.stdout.columns - rowLength.length;
+  const quoteArray = wordWrapToStringList(quoteLength, remain);
+  return quoteArray;
+}
+
+function wordWrapToStringList(text, maxLength) {
+  let result = [],
+    line = [];
+  let length = 0;
+  text.split(" ").forEach(function (word) {
+    if (length + word.length >= maxLength) {
+      result.push(line.join(" "));
+      line = [];
+      length = 0;
     }
-    console.log(el);
+    length += word.length + 1;
+    line.push(word);
   });
+  if (line.length > 0) {
+    result.push(line.join(" "));
+  }
+  return result;
 }
